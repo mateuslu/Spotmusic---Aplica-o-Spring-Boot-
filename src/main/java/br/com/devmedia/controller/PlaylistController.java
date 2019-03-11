3214c71fb42bff91c2f9a65ea417d15b1cd00f87
mateuslu <mateuslupi94@gmail.com>
2019-03-11 16:26:54 -0300

    Implementação do excluir playlist

--- src/main/java/br/com/devmedia/controller/PlaylistController.java
+++ src/main/java/br/com/devmedia/controller/PlaylistController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -43,4 +40,31 @@ public class PlaylistController {
         attr.addFlashAttribute("mensagem", "Playlist criada com sucesso.");
         return "redirect:/playlists/listar";
     }
+
+    @GetMapping("/{id}/atualizar")
+    public ModelAndView preAtualizar(@PathVariable("id") long id, ModelMap model) {
+        Playlist playlist = playlistService.recuperarPoiId(id);
+        model.addAttribute("playlist", playlist);
+        return new ModelAndView("/playlist/add", model);
+    }
+
+    @PutMapping("/salvar")
+    public String atualizar(@Valid @ModelAttribute("playlist") Playlist playlist, BindingResult result, RedirectAttributes attr) {
+        if (result.hasErrors()) {
+            return "/playlist/add";
+        }
+
+        playlistService.atualizar(playlist);
+        attr.addFlashAttribute("mensagem",
+                "Playlist atualizada com sucesso.");
+        return "redirect:/playlists/listar";
+    }
+
+    @GetMapping("/{id}/remover")
+    public String remover(@PathVariable("id") long id, RedirectAttributes attr) {
+        playlistService.excluir(id);
+        attr.addFlashAttribute("mensagem", "Playlist excluída com sucesso.");
+        return "redirect:/playlists/listar";
+    }
+
 }
